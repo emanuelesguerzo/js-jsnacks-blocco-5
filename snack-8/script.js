2983e3ef24ceaf5fbc50a23b740022482915b8e3
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-15 13:51:05 +0100

    feat: snack-8 complete

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,10 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const marco = students.find((curName) => {
+    return curName.name === "Marco Lanci";
+});
+
+console.log(marco.class);
+
